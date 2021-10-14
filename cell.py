d4cc3da1746e7217ad6b4451cdbfd1660346887f
flackobogdan <mi17221@alas.matf.bg.ac.rs>
2021-10-14 22:26:17 +0200

    skelet celije

---
+++ cell.py
@@ -0,0 +1,59 @@
+from itertools import count
+import numpy
+import random
+from grid import *
+
+def get_neighbours(rowNumber, colNumber):
+    result = []
+    for rowAdd in range(-1, 2):
+        newRow = rowNumber + rowAdd
+        if newRow >= 0 and newRow <= len(matrix)-1:
+            for colAdd in range(-1, 2):
+                newCol = colNumber + colAdd
+                if newCol >= 0 and newCol <= len(matrix)-1:
+                    if newCol == colNumber and newRow == rowNumber:
+                        continue
+                    result.append(matrix[newRow][newCol])
+    return result
+
+#cell_state_t = f(neighbours_states_t-1)
+class Cell:
+
+    def __init__(self, i, j):
+        self.rpos = i
+        self.cpos = j
+        self.value = random.choice([True, False])
+        self.neighbours = get_neighbours(self.rval, self.cval)
+
+
+    def count_neighbours(self):
+        count = 0
+
+        for neighbour in self.neighbours:
+            if neighbour.value == True :
+                count += 1
+
+        return count
+
+    # 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    # 2. Any live cell with two or three live neighbours lives on to the next generation.
+    # 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+    # 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+    def next_state(self, neighbours_count):
+
+        if self.value == True :
+            #rule no.1
+            if neighbours_count < 2:
+                self.value = False
+            #rule no.3            
+            if neighbours_count > 3:
+                self.value = False 
+            #else rule no.2 will automatically be satisfied
+
+        if self.value == False:
+            if neighbours_count == 3:
+                self.value = True
+
+    def draw_cell(self):
+        pass
